af1688d7ae0d80746e27c99a0f55c9b41c5bc710
Raman Gugnani <raman.gugnani@paytmmall.com>
2020-12-12 14:12:51 +0530

    Solved DP problem

---
+++ src/main/java/practice/leetcode/problems/HouseRobber_198.java
@@ -0,0 +1,26 @@
+package practice.leetcode.problems;
+
+public class HouseRobber_198 {
+    public static void main(String[] args) {
+        int [] nums = new int[] {1,2,3,1};
+        System.out.println(rob(nums));
+     
+        nums = new int[] {2,7,9,3,1};
+        System.out.println(rob(nums));
+        
+        nums = new int[] {2,1};
+        System.out.println(rob(nums));
+        
+        nums = new int[] {1,2};
+        System.out.println(rob(nums));
+    }
+
+    public static int rob(int[] nums) {
+        for(int i = 0 ; i < nums.length ; i++) {
+            Integer previousValue = i-1 < 0 ? 0 : nums[i-1];
+            Integer previousToPreviousValue = i-2 < 0 ? 0 : nums[i-2];
+            nums[i] = Math.max(previousValue, previousToPreviousValue + nums[i]);
+        }
+        return nums[nums.length-1];
+    }
+}
